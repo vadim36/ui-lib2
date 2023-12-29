62d597d4759d06ecf7764aeaf653e7e8f929d6ce
Vadim Medvedev <vadim130809@gmail.com>
2023-12-29 09:16:58 +0300

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,33 @@
 const path = require('path');
 
-module.exports = {
-  
+module.exports = (ENV) => { 
+  return {
+    mode: ENV.mode ?? 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js',
+      clean: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.tsx?$/i,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    } 
+  }
 }
